70f6790d6fc0e879de336419fc1e5898e2d4ccbf
Olga Panfilova <gr-olga@ya.ru>
2022-02-22 17:32:50 +0100

    recursion

---
+++ recursion.js
@@ -0,0 +1,8 @@
+//Let's create a function that counts down from a given number and output to console in each iteration.
+// for loop isn't allowed, use recursion
+// countDownFrom(5);
+// 5
+// 4
+// 3
+// 2
+// 1
