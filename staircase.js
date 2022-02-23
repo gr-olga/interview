6e003f34ca24422425e8e58bca4735b46af5bee0
Olga Panfilova <gr-olga@ya.ru>
2022-02-23 17:47:18 +0100

    staircase

---
+++ staircase.js
@@ -0,0 +1,21 @@
+// Make a functions that getting a number and output a "staircase".
+// The bottom of the staircase should be a line full of "#" characters.
+// The number of "#" characters in the bottom line is equals to to the given argument,
+// e.g. for staircase(2) the bottom line will be ##, for staircase(4) – ####
+// Each line above should contain 1 "#" less.
+// The very top line should contain only one "#"
+
+
+function staircase(n) {
+
+}
+
+staircase(6)
+
+// expected output for 6:
+// _____#
+// ____##
+// ___###
+// __####
+// _#####
+// ######
