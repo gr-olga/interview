6e7237054c047c023b6eae73e1112aad89e08dd3
Olga Panfilova <gr-olga@ya.ru>
2023-01-27 13:24:18 +0100

    add new item

---
+++ camel.js
@@ -0,0 +1,14 @@
+// String.prototype.toJadenCase = function () {
+//     console.log("string");
+// };
+
+function jadenCase(str) {
+    const newList = str.split(" ")
+    // console.log(newList);
+    const up = newList.map((item) => {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+    })
+    return up.join(" ")
+}
+
+jadenCase("How can mirrors be real if our eyes aren't real")
